cbab083c16e8bbec1c9ca069d59688905755946b
otkulseng <otkulseng1@gmail.com>
2023-03-14 11:02:32 +0100

    ekstra kommentarer

--- Kode/algoritmer.py
+++ Kode/algoritmer.py
@@ -4,17 +4,20 @@ import numpy as np
 
 def plot3DTensegrityStructure(x, p, cables, bars):
     """Generates a 3D image of the tensegrity structure. Usage:
+    // fixed points
     p = np.array([
         [1, 1, 2],
         [2, 2, 1],
         [1, -1, 4]
     ])
 
+    // free points
     x = np.array([
         [0, 0, 0],
         [0, 1, -1],
     ])
 
+    // cables. First element means cable from p0 to x1
     cables = np.array([
         [p[0], x[1]],
         [p[1], x[0]],
@@ -22,6 +25,7 @@ def plot3DTensegrityStructure(x, p, cables, bars):
         [x[1], x[0]]
     ])
 
+    // bars. First element means bar from p0 to x0
     bars = np.array([
         [p[0], x[0]]
     ])
