e9cca67d1a4ed404e84c75672a59ef14c9f3eaaf
otkulseng <otkulseng1@gmail.com>
2023-03-14 22:56:13 +0100

    Ferdig med energifunksjonen

--- Kode/algoritmer.py
+++ Kode/algoritmer.py
@@ -69,21 +69,23 @@ def plot3DTensegrityStructure(x, p, cables, bars):
     return fig, ax
 
 
-def helper_split(p, cables):
-    cable_len = cables[:, -1]
-    cables = np.array(cables[:, :2], dtype=int)
+def cable_elastic(state, cables, k):
+    l = state[cables[:, 0]]
+    r = state[cables[:, 1]]
+    diff = np.linalg.norm(l - r, axis=1) - cables[:, 2]
+    diff = np.where(diff < 0, 0, diff)
+    return (k /(2*cables[:, 2]**2) * diff) @ diff
 
-    # Cables between free points
-    free_free = cables[(cables[:, 1] >= len(p)) & (cables[:, 0] >= len(p))]
-    free_free_len = cable_len[(cables[:, 1] >= len(p)) & (cables[:, 0] >= len(p))]
-    free_free -= len(p) # Index in x¨
+def bar_elastic(state, bars, c):
+    l = state[bars[:, 0]]
+    r = state[bars[:, 1]]
+    diff = np.linalg.norm(l - r, axis=1) - bars[:, 2]
+    return (c /(2*bars[:, 2]**2) * diff) @ diff
 
-    # Cables between free and fixed point
-    fix_free = cables[(cables[:, 1] >= len(p)) & (cables[:, 0] < len(p))]
-    fix_free_len = cable_len[(cables[:, 1] >= len(p)) & (cables[:, 0] < len(p))]
-    fix_free[:, 1] -= len(p)
-    return free_free, free_free_len, fix_free, fix_free_len
-
+def bar_potential(state, bars, rho):
+    l = state[bars[:, 0]]
+    r = state[bars[:, 1]]
+    return 0.5 * rho * bars[:, 2] @ (l[:, 2] + r[:, 2])
 
 def gen_E(p, cables, free_weights, bars, k=2, c=3, rho=0):
     """Takes in the position of all fixed points p,
@@ -94,48 +96,26 @@ def gen_E(p, cables, free_weights, bars, k=2, c=3, rho=0):
         objective function f(x) where x is a numpy array of free
         optimization variables
     """
-
-    ff_cables, ff_cables_len, fx_cables, fx_cables_len = helper_split(p, cables)
-    ff_bars, ff_bars_len, fx_bars, fx_bars_len = helper_split(p, bars)
-
+    state = np.zeros((len(p) + len(free_weights), 3))
+    state[:len(p)] = p
 
     def func(xx):
         x = np.reshape(xx, (-1, 3))
+        state[len(p):] = x
 
-        # Energy of cables between fixed and free point
-        pfree = p[fx_cables[:, 0]]
-        xfree = x[fx_cables[:, 1]]
-        a = np.linalg.norm(pfree-xfree, axis=1) - fx_cables_len
-        a = np.where(a<0, 0, a)
+        # Elastic energy of the cables
+        E_cable_elastic = cable_elastic(state, cables, k)
 
-        Efixfree = (k /(2*fx_cables_len**2) * a) @ a
+        # Elastic energy of the bars
+        E_bar_elastic = bar_elastic(state, bars, c)
 
-        # Energy of cables between free and free point
-        xfree1 = x[ff_cables[:, 0]]
-        xfree2 = x[ff_cables[:, 1]]
-        b = np.linalg.norm(xfree1-xfree2, axis=1) - ff_cables_len
-        b = np.where(b<0, 0, b)
+        # Potential energy of the bars
+        E_bar_potential = bar_potential(state, bars, rho)
 
-        Efreefree = (k /(2*ff_cables_len**2) * b) @ b
+        # Potential energy of all free weights
+        E_ext = free_weights @ x[:, 2]
 
-        # Energy of all free weights
-        Eext = free_weights @ x[:, -1]
-
-        # Energy of bars between fixed and free point
-        pfree1 = p[fx_bars[:, 0]]
-        xfree1 = x[fx_bars[:, 1]]
-        aa = np.linalg.norm(pfree1-xfree1, axis=1) - fx_bars_len
-
-        Ebar_fix_free = (c /(2*fx_bars_len**2) * aa) @ aa
-
-        # Energy of cables between free and free point
-        xfree11 = x[ff_bars[:, 0]]
-        xfree22 = x[ff_bars[:, 1]]
-        bb = np.linalg.norm(xfree11-xfree22, axis=1) - ff_bars_len
-
-        Ebar_free_free = (c /(2*ff_bars_len**2) * bb) @ bb
-
-        return Efixfree + Efreefree + Eext + Ebar_fix_free + Ebar_free_free
+        return E_cable_elastic + E_bar_elastic + E_bar_potential + E_ext
     return func
 
 
--- Kode/main.py
+++ Kode/main.py
@@ -3,13 +3,6 @@ import numpy as np
 from scipy.optimize import minimize
 from algoritmer import plot3DTensegrityStructure, gen_E
 
-
-# x = np.linspace(0, 1, 100)
-# plt.plot(x, -x**2)
-# plt.savefig("./Bilder/oppg1.png")
-# dev = 3
-
-
 # Fixed points
 pp = np.array([
     [1, 1, 0],
@@ -52,10 +45,10 @@ f = gen_E(pp, cables, fw, bars, k=0.1, c=1, rho=0)
 
 
 x0 = np.ones(3 * len(fw))
-res = minimize(f, x0, method='CG')
+res = minimize(f, x0, method='CG', tol=1e-12)
 points = res.x
 sol = np.reshape(points, (-1, 3))
-print(sol)
+print(np.round(sol, 6))
 
 # print(sol)
 
