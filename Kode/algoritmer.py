aa5c1ccb45fda81870eb5c0c1f045b1e2eaaf9e2
otkulseng <otkulseng1@gmail.com>
2023-04-21 10:37:45 +0200

    fjernet strong-wolfe

--- Kode/algoritmer.py
+++ Kode/algoritmer.py
@@ -1,98 +1,6 @@
 import numpy as np
 from scipy.optimize import approx_fprime
 
-
-
-def StrongWolfe(f, gradf, x,p,
-                initial_value,
-                initial_descent,
-                initial_step_length = 1.0,
-                c1 = 1e-2,
-                c2 = 0.9,
-                max_extrapolation_iterations = 50,
-                max_interpolation_iterations = 20,
-                rho = 2.0):
-    '''
-    Implementation of a bisection based bracketing method
-    for the strong Wolfe conditions
-    '''
-    # initialise the bounds of the bracketing interval
-    alphaR = initial_step_length
-    alphaL = 0.0
-    # Armijo condition and the two parts of the Wolfe condition
-    # are implemented as Boolean variables
-    next_x = x+alphaR*p
-    next_value = f(next_x)
-    next_grad = gradf(next_x)
-    Armijo = (next_value <= initial_value+c1*alphaR*initial_descent)
-    descentR = np.inner(p,next_grad)
-    curvatureLow = (descentR >= c2*initial_descent)
-    curvatureHigh = (descentR <= -c2*initial_descent)
-    # We start by increasing alphaR as long as Armijo and curvatureHigh hold,
-    # but curvatureLow fails (that is, alphaR is definitely too small).
-    # Note that curvatureHigh is automatically satisfied if curvatureLow fails.
-    # Thus we only need to check whether Armijo holds and curvatureLow fails.
-    itnr = 0
-    while (itnr < max_extrapolation_iterations and (Armijo and (not curvatureLow))):
-        itnr += 1
-        # alphaR is a new lower bound for the step length
-        # the old upper bound alphaR needs to be replaced with a larger step length
-        alphaL = alphaR
-        alphaR *= rho
-        # update function value and gradient
-        next_x = x+alphaR*p
-        next_value = f(next_x)
-        next_grad = gradf(next_x)
-        # update the Armijo and Wolfe conditions
-        Armijo = (next_value <= initial_value+c1*alphaR*initial_descent)
-        descentR = np.inner(p,next_grad)
-        curvatureLow = (descentR >= c2*initial_descent)
-        curvatureHigh = (descentR <= -c2*initial_descent)
-    # at that point we should have a situation where alphaL is too small
-    # and alphaR is either satisfactory or too large
-    # (Unless we have stopped because we used too many iterations. There
-    # are at the moment no exceptions raised if this is the case.)
-    alpha = alphaR
-    itnr = 0
-    # Use bisection in order to find a step length alpha that satisfies
-    # all conditions.
-    while (itnr < max_interpolation_iterations and (not (Armijo and curvatureLow and curvatureHigh))):
-        itnr += 1
-        if (Armijo and (not curvatureLow)):
-            # the step length alpha was still too small
-            # replace the former lower bound with alpha
-            alphaL = alpha
-        else:
-            # the step length alpha was too large
-            # replace the upper bound with alpha
-            alphaR = alpha
-        # choose a new step length as the mean of the new bounds
-        alpha = (alphaL+alphaR)/2
-        # update function value and gradient
-        next_x = x+alphaR*p
-        next_value = f(next_x)
-        next_grad = gradf(next_x)
-        # update the Armijo and Wolfe conditions
-        Armijo = (next_value <= initial_value+c1*alphaR*initial_descent)
-        descentR = np.inner(p,next_grad)
-        curvatureLow = (descentR >= c2*initial_descent)
-        curvatureHigh = (descentR <= -c2*initial_descent)
-    # return the next iterate as well as the function value and gradient there
-    # (in order to save time in the outer iteration; we have had to do these
-    # computations anyway)
-    return alpha
-
-def other_linesearch(pk,
-               xk,
-               f,
-               gradf,
-               alpha_max = 10,
-               rho = 2,
-               c1 = 1e-2,
-               c2 = 0.9,
-               Niter=100):
-    return StrongWolfe(f, gradf, xk, pk, f(xk), np.dot(gradf(xk), pk))
-
 def lineSearch(pk,
                xk,
                f,
@@ -196,8 +104,6 @@ def bfgs(x0, f, gradf=None, Niter=100, grad_epsilon=1e-10, plot_summary=False, c
 
         if convergence_plot:
             convergence[n] = norm
-
-
 
     if plot_summary:
         if n == Niter:
